ce1e10bf070fa051206a9df3a3f704298ef30a19
gareng <garfeng_gu@163.com>
2019-04-05 22:09:56 +0800

    添加展示界面到docs目录下

---
+++ docs/precache-manifest.600000c3582d94ff849e6e3afbf1ec28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b2d6e77f24525323aec5",
+    "url": "/static/js/main.f119cec1.chunk.js"
+  },
+  {
+    "revision": "833203a6b7b34825536f",
+    "url": "/static/js/2.d96bc8e0.chunk.js"
+  },
+  {
+    "revision": "b2d6e77f24525323aec5",
+    "url": "/static/css/main.bae091b2.chunk.css"
+  },
+  {
+    "revision": "2d2e2d4e842162a2178c113b9e20c735",
+    "url": "/index.html"
+  }
+];
